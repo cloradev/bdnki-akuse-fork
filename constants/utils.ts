bfce770788a97bfcafb38f14f0a6e7ff3c17d063
cloradev <clora7549@gmail.com>
2025-03-19 18:36:16 -0500

    Delete constants directory

--- constants/utils.ts
+++
@@ -1,5 +0,0 @@
-export const SPONSOR_URL = 'https://ko-fi.com/iONDegen';
-export const OPEN_NEW_ISSUE_URL =
-  'https://github.com/cloradev/bdnki-akuse-fork/issues/new';
-export const EPISODES_INFO_URL = 'https://api.ani.zip/mappings?anilist_id=';
-export const SOFAMAXXING_URL = 'https://sofamaxxing.vercel.app';
