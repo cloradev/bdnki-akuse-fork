ffb5fe89749ee8370402a57b2f980abdf2fe8d2a
cloradev <clora7549@gmail.com>
2025-03-19 18:35:37 -0500

    4
    4

---
+++ constants/utils.ts
@@ -0,0 +1,5 @@
+export const SPONSOR_URL = 'https://ko-fi.com/iONDegen';
+export const OPEN_NEW_ISSUE_URL =
+  'https://github.com/cloradev/bdnki-akuse-fork/issues/new';
+export const EPISODES_INFO_URL = 'https://api.ani.zip/mappings?anilist_id=';
+export const SOFAMAXXING_URL = 'https://sofamaxxing.vercel.app';
